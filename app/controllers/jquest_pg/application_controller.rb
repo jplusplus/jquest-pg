c0b27cbf838df9586974471c5357da96adf2c4ab
Pirhoo <hello@pirhoo.com>
2016-06-15 10:33:32 +0200

    Added progression

--- app/controllers/jquest_pg/application_controller.rb
+++ app/controllers/jquest_pg/application_controller.rb
@@ -3,5 +3,13 @@ module JquestPg
     def index
       render 'jquest_pg/index', :layout => 'layouts/application'
     end
+
+    def progression(user=nil)
+      {
+        level: 1,
+        round: 1,
+        user_id: user ? user.id : nil
+      }
+    end
   end
 end
