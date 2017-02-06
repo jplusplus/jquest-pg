aa53a37ba8a20db510190bf136618d8b0fc14c76
pirhoo <hello@pirhoo.com>
2017-02-06 16:24:07 +0100

    Remove schema plus

--- db/migrate/20160606164656_create_jquest_pg_mandature.rb
+++ db/migrate/20160606164656_create_jquest_pg_mandature.rb
@@ -1,8 +1,8 @@
 class CreateJquestPgMandature < ActiveRecord::Migration[5.0]
   def change
     create_table :jquest_pg_mandatures do |t|
-      t.references :legislature, index: true, foreign_key: true, references: :jquest_pg_legislatures
-      t.references :person, index: true, foreign_key: true, references: :jquest_pg_people
+      t.references :legislature, index: true, foreign_key: {:to_table  => :jquest_pg_legislatures}
+      t.references :person, index: true, foreign_key: {:to_table  => :jquest_pg_people}
       t.string :political_leaning
       t.string :role
       t.string :group
--- jquest_pg.gemspec
+++ jquest_pg.gemspec
@@ -27,8 +27,5 @@ Gem::Specification.new do |s|
   s.add_dependency 'active_model_serializers', '~> 0.10.0'
   s.add_dependency 'google_drive'
   s.add_dependency 'tty'
-  # Allow references with a different name (usefull with engine-prefixed tables)
-  # See https://github.com/SchemaPlus/schema_plus_foreign_keys#migrations
-  s.add_dependency 'schema_plus_foreign_keys'
   s.add_development_dependency 'sqlite3'
 end
