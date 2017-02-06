4e81e85bddb98b6ff00f1841aa51cdba5dcf6ad6
pirhoo <hello@pirhoo.com>
2017-02-06 12:11:09 +0100

    Different migration according to adapter

--- db/migrate/20160628152351_create_jquest_pg_diversities.rb
+++ db/migrate/20160628152351_create_jquest_pg_diversities.rb
@@ -2,8 +2,8 @@ class CreateJquestPgDiversities < ActiveRecord::Migration[5.0]
   def change
     create_table :jquest_pg_diversities do |t|
       # All action are relation to a user AND a season
-      t.references :resource_a, polymorphic: true
-      t.references :resource_b, polymorphic: true
+      t.references :resource_a, polymorphic: true, index: false
+      t.references :resource_b, polymorphic: true, index: false
       # Type of the resource this comparaison
       t.string :resource_a_type
       t.string :resource_b_type
@@ -14,5 +14,8 @@ class CreateJquestPgDiversities < ActiveRecord::Migration[5.0]
 
       t.timestamps null: false
     end
+    # Manualy create indexes to set custom names
+    add_index :jquest_pg_diversities, [:resource_a_type, :resource_a_id], :name => :index_pg_diversities_on_resource_a_type_and_resource_a_id
+    add_index :jquest_pg_diversities, [:resource_b_type, :resource_b_id], :name => :index_pg_diversities_on_resource_b_type_and_resource_b_id
   end
 end
--- db/migrate/20160803065133_change_jquest_pg_people_birthdate_to_date.rb
+++ db/migrate/20160803065133_change_jquest_pg_people_birthdate_to_date.rb
@@ -1,5 +1,12 @@
 class ChangeJquestPgPeopleBirthdateToDate < ActiveRecord::Migration[5.0]
   def change
-    change_column :jquest_pg_people, :birthdate, :date
+    case connection.adapter_name.downcase.to_sym
+    when :sqlite
+      change_column :jquest_pg_people, :birthdate, :date
+    when :postgresql
+      change_column :jquest_pg_people, :birthdate, "date USING to_date(birthdate, 'DD/MM/YYYY')"
+    else
+      raise NotImplementedError, "Unknown adapter type"
+    end
   end
 end
