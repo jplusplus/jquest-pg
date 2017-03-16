b4e28c991872373b097f38118624dbaccd7bcb73
pirhoo <hello@pirhoo.com>
2017-03-16 17:09:31 +0100

    Renamed .js.coffee to .coffee

--- app/javascript/packs/jquest_pg/index.js
+++ app/javascript/packs/jquest_pg/index.js
@@ -1,48 +1,48 @@
-import mandatureFormDirective from './components/mandature-form/mandature-form.directive.js.coffee';
-import mandatureFormController from './components/mandature-form/mandature-form.controller.js.coffee';
+import mandatureFormDirective from './components/mandature-form/mandature-form.directive.coffee';
+import mandatureFormController from './components/mandature-form/mandature-form.controller.coffee';
 
-import pgController from './pg/pg.controller.js.coffee';
+import pgController from './pg/pg.controller.coffee';
 
-import pgIntroState from './pg/intro/intro.state.js.coffee';
-import pgIntroConstant from './pg/intro/intro.constant.js.coffee';
-import pgIntroController from './pg/intro/intro.controller.js.coffee';
+import pgIntroState from './pg/intro/intro.state.coffee';
+import pgIntroConstant from './pg/intro/intro.constant.coffee';
+import pgIntroController from './pg/intro/intro.controller.coffee';
 
-import pgLevelState from './pg/level/level.state.js.coffee';
-import pgLevelController from './pg/level/level.controller.js.coffee';
+import pgLevelState from './pg/level/level.state.coffee';
+import pgLevelController from './pg/level/level.controller.coffee';
 
-import pgLevelRoundState from './pg/level/round/round.state.js.coffee';
-import pgLevelRoundController from './pg/level/round/round.controller.js.coffee';
+import pgLevelRoundState from './pg/level/round/round.state.coffee';
+import pgLevelRoundController from './pg/level/round/round.controller.coffee';
 
-import pgLevelRoundIntroState from './pg/level/round/intro/intro.state.js.coffee';
-import pgLevelRoundIntroController from './pg/level/round/intro/intro.controller.js.coffee';
+import pgLevelRoundIntroState from './pg/level/round/intro/intro.state.coffee';
+import pgLevelRoundIntroController from './pg/level/round/intro/intro.controller.coffee';
 
-import pgLevelRoundGenderState from './pg/level/round/gender/gender.state.js.coffee';
-import pgLevelRoundGenderController from './pg/level/round/gender/gender.controller.js.coffee';
-import pgLevelRoundGenderSummaryState from './pg/level/round/gender/summary/summary.state.js.coffee';
-import pgLevelRoundGenderSummaryController from './pg/level/round/gender/summary/summary.controller.js.coffee';
+import pgLevelRoundGenderState from './pg/level/round/gender/gender.state.coffee';
+import pgLevelRoundGenderController from './pg/level/round/gender/gender.controller.coffee';
+import pgLevelRoundGenderSummaryState from './pg/level/round/gender/summary/summary.state.coffee';
+import pgLevelRoundGenderSummaryController from './pg/level/round/gender/summary/summary.controller.coffee';
 
-import pgLevelRoundDetailsState from './pg/level/round/details/details.state.js.coffee';
-import pgLevelRoundDetailsController from './pg/level/round/details/details.controller.js.coffee';
-import pgLevelRoundDetailsSummaryState from './pg/level/round/details/summary/summary.state.js.coffee';
-import pgLevelRoundDetailsSummaryController from './pg/level/round/details/summary/summary.controller.js.coffee';
+import pgLevelRoundDetailsState from './pg/level/round/details/details.state.coffee';
+import pgLevelRoundDetailsController from './pg/level/round/details/details.controller.coffee';
+import pgLevelRoundDetailsSummaryState from './pg/level/round/details/summary/summary.state.coffee';
+import pgLevelRoundDetailsSummaryController from './pg/level/round/details/summary/summary.controller.coffee';
 
-import pgLevelRoundDiversityState from './pg/level/round/diversity/diversity.state.js.coffee';
-import pgLevelRoundDiversityController from './pg/level/round/diversity/diversity.controller.js.coffee';
+import pgLevelRoundDiversityState from './pg/level/round/diversity/diversity.state.coffee';
+import pgLevelRoundDiversityController from './pg/level/round/diversity/diversity.controller.coffee';
 
 
-import pgDataState from './pg/data/data.state.js.coffee';
-import pgDataController from './pg/data/data.controller.js.coffee';
+import pgDataState from './pg/data/data.state.coffee';
+import pgDataController from './pg/data/data.controller.coffee';
 
-import pgRepresentativesEditController from './pg/representatives/edit/edit.controller.js.coffee';
-import pgRepresentativesEditState from './pg/representatives/edit/edit.state.js.coffee';
+import pgRepresentativesEditController from './pg/representatives/edit/edit.controller.coffee';
+import pgRepresentativesEditState from './pg/representatives/edit/edit.state.coffee';
 
-import pgRepresentativesController from './pg/representatives/representatives.controller.js.coffee';
-import pgRepresentativesState from './pg/representatives/representatives.state.js.coffee';
+import pgRepresentativesController from './pg/representatives/representatives.controller.coffee';
+import pgRepresentativesState from './pg/representatives/representatives.state.coffee';
 
-import pgState from './pg/pg.state.js.coffee';
-import pgConstant from './pg/pg.constant.js.coffee';
-import pgRun from './pg/pg.run.js.coffee';
-import pg204State from './pg/204/204.state.js.coffee';
+import pgState from './pg/pg.state.coffee';
+import pgConstant from './pg/pg.constant.coffee';
+import pgRun from './pg/pg.run.coffee';
+import pg204State from './pg/204/204.state.coffee';
 
 import './index.scss';
 
